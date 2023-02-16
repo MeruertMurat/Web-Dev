62ac33e5374109c36a2a2b489038dec2987811ba
MeruertMurat <m_murat@kbtu.kz>
2023-02-16 15:37:55 +0600

    main

---
+++ Lab3/JavaScript Info/Fundamentals/16.js
@@ -0,0 +1,5 @@
+alert( null || 2 || undefined );
+
+alert( alert(1) || 2 || alert(3) );
+
+alert(1 && null && 2);
---
+++ Lab3/JavaScript Info/Fundamentals/23.js
@@ -0,0 +1,24 @@
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    }
+    // ...
+    return confirm('Did parents allow you?');
+  }
+
+  function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      // ...
+      return confirm('Did parents allow you?');
+    }
+  }
+
+  function checkAge(age) {
+    return (age > 18) ? true : confirm('Did parents allow you?');
+  }
+
+  function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+  }
